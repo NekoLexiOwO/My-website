f478e688c961b0a06ffc1181388004268342d482
Alexis Woods <118643214+NekoLexiOwO@users.noreply.github.com>
2023-09-26 11:39:57 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,4 @@
+const headinga = document.queryelector("h1");
+headinga. textContent = "welcome to my website!"
+// comment line 
+
---
+++ python.py
@@ -0,0 +1,9 @@
+print(1 + 4)
+# will print 5
+
+print("hello world!")
+# will print the phrase hello world
+
+apple = 6 * 3
+print(apple)
+# will print 18
